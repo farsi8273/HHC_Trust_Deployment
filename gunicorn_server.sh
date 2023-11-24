d9a01a8fb6a53a439a7d4e0e10737aac8b67de67
salman <salman123@gmai.com>
2023-11-24 16:38:23 +0530

    update

--- gunicorn_server.sh
+++ gunicorn_server.sh
@@ -13,14 +13,15 @@ BIND_ADDRESS="0.0.0.0"
 BIND_PORT=8000
 
 # Gunicorn log file
-LOG_FILE= "/var/log/gunicorn/dev.log"
+LOG_FILE= "gunicorn/dev.log"
 
 # PID file for Gunicorn process
 PID_FILE="gunicorn.pid"
 
 # Activate your virtual environment (if applicable)
 # source /path/to/your/virtualenv/bin/activate
-
+# mkdir -p /var/log/gunicorn/
+# touch /var/log/gunicorn/dev.log
 # Start Gunicorn
 gunicorn \
     --workers $WORKERS \
