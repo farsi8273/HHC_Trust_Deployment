ee73ba5b46cf842d48277fd3e0231710f16a6d9e
salman <salman123@gmai.com>
2023-11-25 10:58:18 +0530

    static content update

--- ip_finder.sh
+++ ip_finder.sh
@@ -1,5 +1,5 @@
 public_ip=$(curl -s ifconfig.me)
-echo "getting public ip.........: $(public_ip)"
+echo "Getting public ip.........: $(public_ip)"
 sudo sed -i "s/server_name.*/server_name $public_ip;/" /etc/nginx/sites-available/hhcproject
 sudo ln -s /etc/nginx/sites-available/hhcproject /etc/nginx/sites-enabled
 sudo nginx -t
