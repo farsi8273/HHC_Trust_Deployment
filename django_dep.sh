9491624aa9e6bd5c6e0ba68af5565486909c4a5d
salman <salman123@gmai.com>
2023-11-25 10:31:25 +0530

    configuration level changes applied

--- django_dep.sh
+++ django_dep.sh
@@ -2,41 +2,27 @@
 # Project specific environment vars
 echo 'Intializing env vars...........'
 source ./env.sh
-# db_host="hhcdjango.ctbmd1d7wwum.eu-north-1.rds.amazonaws.com"
-# db_port="5432"
-# db_name="hhcdb"
-# db_user="postgres"
-# db_pass="postgres123"
-# # emil app details
-# email='hhc4you@gmail.com'
-# email_app_key='ntudgjozyuznzyms'
-# #gdal environment veriable
-# CPLUS_INCLUDE_PATH=/usr/include/gdal
-# C_INCLUDE_PATH=/usr/include/gdal
-# # django super user details
-# DJANGO_SUPERUSER_PASSWORD=hhc4you
-echo "Installing OS packages.."
+echo "Installing OS packages......"
 sudo add-apt-repository -y ppa:ubuntugis/ppa 
 sudo apt update -y
-sudo apt install -y python3-venv python3-dev libpq-dev postgresql postgresql-contrib nginx curl postgis postgresql-14-postgis-3 python3-pip libgdal-dev gdal-bin
+sudo apt install -y python3-venv python3-dev libpq-dev postgresql postgresql-contrib curl postgis postgresql-14-postgis-3 python3-pip libgdal-dev gdal-bin
 # git clone https://github.com/farsi8273/HHC_Trust_Deployment.git
 # cd HHC_Trust_Deployment/
 
-echo "Creating  python environment.........."
+echo "Creating  Python environment.........."
 python3 -m venv hhcenv
 source ./hhcenv/bin/activate
 echo 'Installing required project dependencies.......'
 pip install -r requirements.txt
-echo "running python project.."
+echo "running python project....."
 python3 manage.py makemigrations
-echo 'creating migration............'
+echo 'creating migration...........'
 python3 manage.py migrate
-echo ' migrate done.............'
+echo ' migrate command in completed.............'
 USERNAME="admin"
 EMAIL="hhc4you@gmail.com"
 # Check if the superuser already exists
 existing_superuser=$(python3 manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$USERNAME').exists())")
-
 if [ "$existing_superuser" = "False" ]; then
     # Create the superuser
     python3 manage.py createsuperuser --username=$USERNAME --email=$EMAIL --noinput
@@ -45,14 +31,16 @@ else
     echo "Superuser '$USERNAME' already exists."
 fi
 # python3 manage.py createsuperuser --noinput --username admin --email $email
-echo 'before collectstatic...........'
+echo 'Before collectstatic...........'
 python3 manage.py collectstatic --noinput
-echo ' creating statics............'
+echo 'collectstatic completed successfully.....'
 sudo ufw allow 8000
-# touch gunicorn-socket.sock
-# chmod 644 gunicorn-socket.sock
-nohup gunicorn -c gunicorn_conf.py &
-# Nginx Part
+echo "Creating gunicorn Service file...."
+sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+# Nginx installation and configuration
+sudo apt install -y nginx
 public_ip=$(curl -s ifconfig.me)
 echo "getting public ip.........: $(public_ip)"
 sed -i "s/server_name.*/server_name $public_ip;/" ./hhc-server.conf
@@ -61,4 +49,7 @@ sudo ln -s /etc/nginx/sites-available/hhcproject /etc/nginx/sites-enabled
 sudo nginx -t
 sudo systemctl restart nginx
 sudo ufw allow 'Nginx Full'
+sudo cp myscript.service /etc/systemd/system/myscript.service
+sudo systemctl enable myscript
+
 
--- env.sh
+++ env.sh
@@ -1,12 +1,3 @@
-# Projrct specific environment vars
-export db_host="hhcdjango.ctbmd1d7wwum.eu-north-1.rds.amazonaws.com"
-export db_port="5432"
-export db_name="hhcdb"
-export db_user="postgres"
-export db_pass="postgres123"
-# emil app details
-export email='hhc4you@gmail.com'
-export email_app_key='ntudgjozyuznzyms'
 #gdal environment veriable
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
